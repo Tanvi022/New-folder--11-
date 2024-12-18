3c62e835a852e6034bc06f7093a4207c574f2dc0
tanviparab <139870964+Tanvi022@users.noreply.github.com>
2024-12-19 01:00:45 +0530

    Update form.py

--- form.py
+++ form.py
@@ -1,13 +1,8 @@
 import sqlite3
 
 def create_database():
-    # Create a connection to the database (it will create the file if it doesn't exist)
     conn = sqlite3.connect('Form.db')
-    
-    # Create a cursor object
     cursor = conn.cursor()
-    
-    # Create the Student table if it does not exist
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS Student (
             Fullname TEXT,
@@ -18,11 +13,11 @@ def create_database():
         )
     ''')
     
-    # Commit changes and close the connection
+    
     conn.commit()
     conn.close()
 
-# Call the function to create the database and table
+
 create_database()
 
 print("Database and table created successfully!")
